03459e79ce04dec4029016e2933d035f77a202e2
Harish Krishna <hakr@microsoft.com>
2019-10-03 18:23:54 -0700

    Added captions, object bounding boxes visible on canvas, tags

--- ReactWebCamComputerVision/ClientApp/src/components/WebCamCV.js
+++ ReactWebCamComputerVision/ClientApp/src/components/WebCamCV.js
@@ -18,10 +18,13 @@ export class WebCamCV extends Component {
             facingMode: "environment",
             img: null,
             objects: null,
+            tags: null,
+            caption: null,
         };
         this.makeblob = this.makeblob.bind(this);
         this.capture = this.capture.bind(this);
-        this.toggle = this.toggle.bind(this)
+        this.toggle = this.toggle.bind(this);
+        this.updateCanvas = this.updateCanvas.bind(this);
     }
 
     makeblob = function (dataURL) {
@@ -45,11 +48,47 @@ export class WebCamCV extends Component {
         return new Blob([uInt8Array], { type: contentType });
     }
 
+    updateCanvas() {
+        const ctx = this.canvas.getContext('2d');
+        var img = new Image;
+        let objects = this.state.objects;
+
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0);
+
+            ctx.lineWidth = 4;
+            ctx.lineJoin = 'bevel';
+            const font = "16px sans-serif"
+            ctx.font = font;
+            ctx.textBaseline = "top";
+
+            objects.forEach(object => {
+                console.log(object.confidence, object.object, object.rectangle);
+
+                ctx.strokeStyle = "rgba(255,0,0," + String((object.confidence-0.4)*2) +")";
+                ctx.lineWidth = 4;
+                ctx.strokeRect(object.rectangle.x, object.rectangle.y, object.rectangle.w, object.rectangle.h);
+
+                ctx.fillStyle = "rgba(255,255,255," + String(object.confidence) + ")";;
+                const textWidth = ctx.measureText(object.object + " (" + object.confidence + ")").width;
+                const textHeight = parseInt(font, 10); // base 10
+                ctx.fillRect(object.rectangle.x, object.rectangle.y, textWidth + 4, textHeight + 4);
+
+                ctx.fillStyle = "rgb(0,0,0)";
+                ctx.fillText(object.object + " (" + object.confidence + ")", object.rectangle.x, object.rectangle.y);
+
+            });
+        };
+
+        img.src = this.state.img;
+    }
+
     capture = function () {
         const image = this.webcam.getScreenshot();
+        const imageBlob = this.makeblob(image);
         this.setState({ img: image });
 
-
+        //Object Detection
         fetch('https://westus.api.cognitive.microsoft.com/vision/v2.0/detect/', {
             method: 'POST',
             headers: {
@@ -57,15 +96,36 @@ export class WebCamCV extends Component {
                 'Content-Type': 'application/octet-stream',
             },
 
-            body: this.makeblob(image),
+            body: imageBlob,
 
         }).then(response => response.json())
             .then(data => {
-                console.log(data);
                 this.setState({ objects: data.objects });
-            })
+            }).then(returnValue => this.updateCanvas());
+
+
+        //Image description
+        fetch('https://westus.api.cognitive.microsoft.com/vision/v2.0/describe/', {
+            method: 'POST',
+            headers: {
+                'Ocp-Apim-Subscription-Key': '',
+                'Content-Type': 'application/octet-stream',
+            },
+
+            body: imageBlob,
+
+        }).then(response => response.json())
+            .then(data => {
+                console.log(data.description);
+                this.setState({ tags: data.description.tags });
+                if (data.description.captions.length >= 1) {
+                    this.setState({ caption: data.description.captions[0].text });
+                    this.setState({ captionConfidence: data.description.captions[0].confidence.toFixed(3) });
+                }
+            });
 
     }
+
 
     toggle = () => {
         if (this.state.facingMode == "user") {
@@ -81,7 +141,7 @@ export class WebCamCV extends Component {
         return (
             <div>
 
-                <h1>In-browser webcam computer vision with Cognitive Services and React</h1>
+                <h1>In-browser webcam computer vision with Microsoft Azure Cognitive Services and React</h1>
 
                 <p></p>
                 <table>
@@ -104,13 +164,26 @@ export class WebCamCV extends Component {
                                 </center>
                             </td>
                             <td>
-                                {this.state.img ? <img ref="image" src={this.state.img} className="hidden" /> : null}
+                                <canvas ref={(canvas) => this.canvas = canvas} width="512" height="300" />
+                                
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                {this.state.caption ? <p>Caption: {this.state.caption} ({this.state.captionConfidence}) </p> : null}
+                                {this.state.tags ? <div> <h3> Tags </h3> <ul>
+                                    {this.state.tags.map(function (tag, index) {
+                                        return <li key={index}>{tag}</li>;
+                                    })}
+                                </ul> </div> : null}
                             </td>
                         </tr>
                     </tbody>
                 </table>
-                {this.state.objects ? console.log(this.state.objects) : null}
-                <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+
           </div>
         );
   }
