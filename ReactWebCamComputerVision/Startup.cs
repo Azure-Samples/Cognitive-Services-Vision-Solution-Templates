d9ab36e3c819bfefb28f7a24aa0c1f759e3d9c12
Harish <hakr@microsoft.com>
2019-10-08 10:36:57 -0700

    Retargeted to ASP.NET core v2.1

--- ReactWebCamComputerVision/Startup.cs
+++ ReactWebCamComputerVision/Startup.cs
@@ -20,7 +20,7 @@ namespace ReactWebCamComputerVision
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
